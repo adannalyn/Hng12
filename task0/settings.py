1ade3dcef0741e78237670de8f44fd799f7d9192
adannalyn <qvexvon@gmail.com>
2025-02-06 13:02:15 +0100

    new_task 7

--- task0/settings.py
+++ task0/settings.py
@@ -117,6 +117,11 @@ USE_I18N = True
 
 USE_TZ = True
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        }
+    }
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
